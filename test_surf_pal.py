95def7d88e69f072d114c307791290d73ab43e2c
Agata Martyna <agatamartynachalupka@gmail.com>
2020-11-01 22:47:45 +0100

    Add test_surf_pal

---
+++ test_surf_pal.py
@@ -0,0 +1,19 @@
+samples = {
+    'biały':'bʲjawɨ',
+    'ziemia':'ʑɛmʲja',
+    'robi': 'rɔbʲi',
+    'więdnie': 'vʲj ̃ɛ̃w dɲɛ',
+    'powie': 'pɔvʲjɛ',
+    'kwiatki': 'kfʲjatkʲi',
+    'wiewiórka': 'vʲjɛvʲjurka',
+    'kibitki': 'kʲibʲitkʲi',
+    'wiwisekcja': 'vʲivʲisɛktsja',
+    'kitki': 'kʲitkʲi',
+    'operetki': 'ɔpɛrɛtkʲi',
+    'kobitki':'kɔbʲitkʲi'
+}
+
+from  ipa_transcribe_polish import transcribe
+
+for sample in samples:
+    print(sample, '-', transcribe(sample), '-', samples[sample])
