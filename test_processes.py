ae86d575d3c5c05032b6186d0e17e8f5f334e159
Agata Martyna <agatamartynachalupka@gmail.com>
2020-11-12 00:29:15 +0100

    Add unittest for phonological processes

---
+++ test_processes.py
@@ -0,0 +1,48 @@
+import unittest
+from ipa_transcribe_polish import transcribe
+
+
+class TestProcesses(unittest.TestCase):
+
+    def test_final_devoicing(self):
+        self.assertEqual(transcribe('standard'), 'standart')
+        self.assertEqual(transcribe('wąż'), 'ṽɔ̃wʂ')
+        self.assertEqual(transcribe('bug'), 'buk')
+
+    def test_denasalise_nasal_assimilation(self):
+        self.assertEqual(transcribe('mąka'), 'mɔŋka')
+        self.assertEqual(transcribe('gęsty'), 'ɡ̃ɛ̃wstɨ')
+        self.assertEqual(transcribe('rąbać'), 'rɔmbatɕ')
+        self.assertEqual(transcribe('podjęli'), 'pɔdʲjɛlʲi')
+        self.assertEqual(transcribe('pieniądz'), 'pʲjɛɲɔɳts')
+        self.assertEqual(transcribe('pędzić'), 'pɛɲdʑitɕ')
+        self.assertEqual(transcribe('pójdą'), 'pujd̃ɔ̃w')
+        self.assertEqual(transcribe('kręty'), 'krɛntɨ')
+        self.assertEqual(transcribe('pójdę'), 'pujdɛ')
+
+    def test_progressive_voicing_morphological(self):
+        self.assertEqual(transcribe('jakże'), 'jaɡʐɛ')
+        self.assertEqual(transcribe('wszakżeż'), 'fʂaɡʐɛʂ')
+        self.assertEqual(transcribe('paśże'), 'paʑʐɛ')
+
+    def test_progressive_devoicing(self):
+        self.assertEqual(transcribe('świat'), 'ɕfʲjat')
+        self.assertEqual(transcribe('przetrzyma'), 'pʂɛtʂɨma')
+        self.assertEqual(transcribe('wycwanić'), 'vɨtsfaɲitɕ')
+
+    def test_progressive_voicing(self):
+        self.assertEqual(transcribe('poczdam'), 'pɔdʐdam')
+        self.assertEqual(transcribe('prośba'), 'prɔʑba')
+
+    def test_regressive_devoicing(self):
+        self.assertEqual(transcribe('babka'), 'bapka')
+        self.assertEqual(transcribe('rozpacz'), 'rɔspatʂ')
+
+    def surface_palatalisation(self):
+        self.assertEqual(transcribe('kibitki'), 'kʲibʲitkʲi')
+        self.assertEqual(transcribe('kwiatki'), 'kfʲjatkʲi')
+        self.assertEqual(transcribe('kolie'), 'kɔlʲjɛ')
+        self.assertEqual(transcribe('robi'), 'rɔbʲi')
+
+if __name__ == '__main__':
+    unittest.main()
